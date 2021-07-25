5463ff00c2684c4c5e2582c1b5a4c133bc90c8cb
Alex <longnamesrule@gmail.com>
2021-07-24 22:14:35 -0400

    updated test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,5 @@
 $Uri = 'http://127.0.0.1:8080/redis'
 $Body = '{"key":"testkey", "value": "testvalue"}'
+Invoke-WebRequest -Uri "http://127.0.0.1:8080/redis&key=testkey"
+Start-Sleep 1
 Invoke-RestMethod -Uri $Uri -Method Post -ContentType "application/json" -Body $Body
-Invoke-WebRequest -Uri "http://127.0.0.1:8080/redis&key=test"
