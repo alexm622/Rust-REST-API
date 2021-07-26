5d974214af63eddfc2974d6be5bdde1edda4d5d4
Alex <longnamesrule@gmail.com>
2021-07-25 21:11:03 -0400

    the http get request function has been ironed out, going to write tests for the function soon

--- src/rest_server/httpGet.rs
+++ src/rest_server/httpGet.rs
@@ -13,28 +13,51 @@ pub mod api_requests {
         pub url: String,
     }
 
-    //this works
-    //and I dont wanna know how
-    //need to do an exorcism on this function later
-    //probably write a test for this function later buuut
-    //no
-    async fn get() -> Result<String, ()>{
+    
+    async fn get(link: &str) -> Result<String, ()>{
+        //create a dns resolver
         let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
-        let ip_addr = resolver.lookup_ip("httpbin.org").unwrap().iter().next().unwrap().to_string();
-        let mut url: String = "http://".to_owned();
+
+        //get the protocol from the url as arg
+        let split_proto = link.split("//").collect::<Vec<&str>>();
+
+        //split on "/"
+        let split = split_proto[1].split("/").collect::<Vec<&str>>();
+
+        //get the ip address of the url
+        let ip_addr = resolver.lookup_ip(split[0]).unwrap().iter().next().unwrap().to_string();
+        
+        //create the starting of the url
+        let mut url: String = split_proto[0].to_owned();
+        url.push_str("//");
         url.push_str(&ip_addr[..]);
-        url.push_str("/get");
+
+        //add all the missing pieces/parts of the url
+        for s in split{
+            url.push_str("/");
+            url.push_str(s);
+        }
+
+        //get ready for the http/https request
         let client = Client::default();
-        let resp = client.get(url)
-            .send().await
-            .ok().unwrap().body()
-            .await.unwrap().to_vec();
-        let response: String = String::from_utf8(resp.clone()).unwrap();
-        Ok(response)
+        let resp = client.get(url).send().await;
+
+        //deal with the results of the request
+        if resp.is_err(){
+            let err = resp.err().unwrap();
+            log::warn!("error encountered. that error was{}", err.to_string());
+
+            Ok("error".to_owned())
+        }else{
+            let vec = resp.ok().unwrap().body().await.unwrap().to_vec();
+            let response: String = String::from_utf8(vec.clone()).unwrap();
+            Ok(response)
+        }
+        
     }
 
     pub async fn api_request() -> HttpResponse{
-        let res: String = get().await.unwrap();
+        let res: String = get("http://httpbin.org/get").await.unwrap();
         HttpResponse::Ok().json( res)
     }
     
